50c0b264bb2f0ceff1c6c28e690b74b91c43be48
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-06-11 21:40:53 +0900

    Creating Migration, Model, Controller, Factories

---
+++ database/factories/QustionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Qustion;
+use Faker\Generator as Faker;
+
+$factory->define(Qustion::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_06_11_102607_create_qustions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQustionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qustions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('qustions');
+    }
+}
