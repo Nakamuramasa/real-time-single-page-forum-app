d20e41048d1733a0bb4ba647d42944879d9044cd
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-06-13 02:25:16 +0900

    CRUD for Category

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -27,7 +27,6 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //auth()->user()->question()->create($request->all());
         Question::create($request->all());
         return response('Created', Response::HTTP_CREATED);
     }
@@ -52,7 +51,8 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return response('Updated', Response::HTTP_ACCEPTED);
     }
 
     /**
